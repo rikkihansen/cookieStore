eecab03bb532d8aa1d24d770524deeb3ab3f3899
Rikki Hansen <rikkihansen@Rikkis-MacBook-Air.local>
2016-03-24 19:05:46 -0700

    cookie shop JS

---
+++ cookie-shop.js
@@ -0,0 +1,51 @@
+// cookie-shop Start
+var pionnerSquare = {
+  minCustomer= 17,
+  maxCustomer= 88,
+  avgCookieSale= 5.2
+};
+ pionnerSquare.customerPerHour=function() {
+     return math.floor(math.random() this.minCustomer =< this.maxCustomer);
+ }
+
+ var portlandAirport = {
+   minCustomer= 6,
+   maxCustomer= 24,
+   avgCookieSale= 1.2
+ };
+
+ portlandAirport.customerPerHour =function() {
+    return math.floor(math.random() this.minCustomer =< this.maxCustomer);
+ }
+
+var washingtonSquare = {
+   minCustomer= 11,
+   maxCustomer= 38,
+   avgCookieSale= 1.2
+ };
+
+ washingtonSquare.customerPerHour =function() {
+      return math.floor(math.random() this.minCustomer =< this.maxCustomer);
+ }
+
+ var sellwood = {
+   minCustomer= 20,
+   maxCustomer= 48,
+   avgCookieSale= 3.3
+ };
+
+ sellwood.customerPerHour =function() {
+      return math.floor(math.random() this.minCustomer =< this.maxCustomer);
+ }
+
+ var pearlDistrict= {
+   minCustomer= 3,
+   maxCustomer= 24,
+   avgCookieSale= 2.6
+ };
+
+ pearlDistrict.customerPerHour = function() {
+        return math.floor(math.random() this.minCustomer =< this.maxCustomer);
+ }
+
+customerPerHour();
